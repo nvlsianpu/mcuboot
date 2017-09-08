eba30eb5fc8fc6401f0fc90d4e0c50b1a877f7e3
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2017-09-08 16:49:14 +0200

    enable serial recovery functionality on the zephyr mcuboot
    This patch introduced serial bootloader functionality ported
    from mynewt targets tree.
    
    For achieving this following changes were applied:
    - Modified boot_serial module for using, zephyr-os modules
      (crc driver, mbedtls-base64 library) and the zephyr serial adapter module
      introduced recently.
    - Added service of boot serial recovery mode to main.
    - Adapted the input parser to using static buffers.
    - Added defines for map to memory allocation API of zephyr.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- boot/zephyr/include/os/os_heap.h
+++ boot/zephyr/include/os/os_heap.h
@@ -20,19 +20,34 @@
 #ifndef H_OS_HEAP_
 #define H_OS_HEAP_
 
+#ifdef __ZEPHYR__
+#include <zephyr.h>
+#else
 #include <stddef.h>
+#endif // __ZEPHYR__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifdef __ZEPHYR__
+
+#define os_malloc k_malloc
+#define os_free k_free
+#define os_realloc k_realloc
+
+#else
+
 void *os_malloc(size_t size);
 void os_free(void *mem);
 void *os_realloc(void *ptr, size_t size);
 
+#endif // __ZEPHYR__
+
 #ifdef __cplusplus
 }
 #endif
+
 
 #endif
 
