d0887c81136e80c7cb16a5550142f9f0f84e1b1e
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2020-08-13 09:22:43 +0200

    downstream modification

--- boot/zephyr/main.c
+++ boot/zephyr/main.c
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ /* downstream modification*/
 #include <assert.h>
 #include <zephyr.h>
 #include <drivers/gpio.h>
