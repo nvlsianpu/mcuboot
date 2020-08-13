7496f4c930ce8bd6f54e1a178cca4794560e589a
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2020-08-13 09:11:49 +0200

    upstream modificatio

--- boot/zephyr/main.c
+++ boot/zephyr/main.c
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ /* upsteram mdification*/
 #include <assert.h>
 #include <zephyr.h>
 #include <drivers/gpio.h>
